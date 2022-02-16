7c0cd5a1689d40a89d2312f35df1ef9951e49434
iqbal999 <iqbal.hossain9@gmail.com>
2022-02-16 19:11:55 +0600

    Wroking on UserProfile with extension method

---
+++ BlazorServerDemo/ClaimsPrincipalExtentions.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorServerDemo
+{
+    public static class ClaimsPrincipalExtentions
+    {
+        public static string GetLoggedInUserEmail(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+
+            string userEmail = principal.FindFirstValue(ClaimTypes.Email);
+            return userEmail;
+        }
+
+        //public static IdentityUser<T> GetLoggedInUser(this ClaimsPrincipal principal, SignInManager<T> signInManager)
+        //{
+        //    if (principal == null)
+        //        throw new ArgumentNullException(nameof(principal));
+
+        //    string userEmail = principal.FindFirstValue(ClaimTypes.Email);
+
+        //    return userEmail;
+        //}
+    }
+}
